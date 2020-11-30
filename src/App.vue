2036342fff7d9d1640f18fc34b71c3664b0b0177
ChoWonmin <0108257@gmail.com>
2020-11-30 21:48:40 +0900

    header

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,18 @@
   v-app
     v-app-bar(app color="primary" dark)
       .d-flex.align-center(
-        alt="Vuetify Logo"
         class="shrink mr-2"
         contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
         transition="scale-transition"
         width="40")
-        v-img(
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        )
-        v-img(
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        )
+        .text-h4  wonmin
       v-spacer
       v-btn(
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       )
-        span.mr-2 Latest Release
+        span.mr-2 GITHUB
         v-icon mdi-open-in-new
     v-main
       router-view

26f0ad37bc7313b5f335d093bc75315d23fb7aa7
ChoWonmin <0108257@gmail.com>
2020-12-04 20:13:39 +0900

    sidebar-content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
   .home
     .side-bar
+      v-tabs(color="#163167" v-model="tab")
+        v-tab Profile
+        v-tab Project
+      .side-bar-content
     .content
 
 </template>
@@ -10,16 +14,25 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private tab = 0;
+}
 </script>
 
 <style lang="sass" scoped>
 .home
   width: 100%
-  height: 100%
+  height: 100vh
   display: flex
   .side-bar
     width: 360px
-    height: 100%
+    height: calc(100vh -64px)
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12) !important
+    overflow-y: scroll
+    .side-bar-content
+      width: 100%
+      height: 1360px
+      background-color: red
+
   .content
 </style>
